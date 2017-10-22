9cd5c5807f94d303924f3bd1c11400021420cfa8
pupate.yang <pupate.yang@vipshop.com>
2017-10-22 23:51:32 +0800

    feat(once): Add once method.

--- easyEvent.js
+++ easyEvent.js
@@ -3,36 +3,44 @@
  * @description A simple implementation of custom event
  */
 
-function EasyEvent(){
-	this.handleTree = {};
+function EasyEvent() {
+    this.handleTree = {};
 };
 
-EasyEvent.prototype.on = function(eventType, handle){
-	if(typeof eventType === 'string'){
-		this.handleTree[eventType] 
-		? ''
-		: this.handleTree[eventType] = [];
-	} else {
-		throw new Error('The event to be monitored must be an String type');
-	}
-	if(typeof handle === 'function'){
-		this.handleTree[eventType].push(handle);
-	} else {
-		throw new Error('The second param of on must be a function');
-	}
+EasyEvent.prototype.on = function (eventType, handle) {
+    if (typeof eventType === 'string') {
+        this.handleTree[eventType]
+            ? ''
+            : this.handleTree[eventType] = [];
+    } else {
+        throw new Error('The event to be monitored must be an String type');
+    }
+    if (typeof handle === 'function') {
+        this.handleTree[eventType].push(handle);
+    } else {
+        throw new Error('The second param of on must be a function');
+    }
 }
 
-EasyEvent.prototype.emit = function(event, data){
-	if(!this.handleTree[event]) return;
-	var handlers = this.handleTree[event];
-	setTimeout(function(){
-		for(var i = 0; i < handlers.length; i++){
-			handlers[i](data)
-		}
-	},0)
+EasyEvent.prototype.once = function (eventType, handle) {
+    var onceFn = function(){
+        this.off(eventType)
+        handle.apply(arguments)
+    }
+    this.on(eventType, onceFn)
 }
 
-EasyEvent.prototype.off = function(event){
-	if(!this.handleTree[event]) return;
-	delete this.handleTree[event];
+EasyEvent.prototype.emit = function (event, data) {
+    if (!this.handleTree[event]) return;
+    var handlers = this.handleTree[event];
+    setTimeout(function () {
+        for (var i = 0; i < handlers.length; i++) {
+            handlers[i](data)
+        }
+    }, 0)
+}
+
+EasyEvent.prototype.off = function (event) {
+    if (!this.handleTree[event]) return;
+    delete this.handleTree[event];
 }
